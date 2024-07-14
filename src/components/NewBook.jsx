b4c265b7a52653bd263445cb61be95d84d3fb2eb
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-07-14 12:52:01 +0200

    Part 8.10 - Implement adding a book

--- src/components/NewBook.jsx
+++ src/components/NewBook.jsx
@@ -1,4 +1,6 @@
+import { useMutation } from '@apollo/client'
 import { useState } from 'react'
+import { ADD_BOOK, ALL_BOOKS, ALL_AUTHORS } from './queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -6,6 +8,18 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const [addBook] =  useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    onError: (error) => {
+      const messages = error.graphQLErrors.map(e => e.message).join('\n') || "Could not add book, please try again later."
+
+      setErrorMessage(messages)
+      setTimeout(() => setErrorMessage(null), 2000)
+    }
+  })
 
   if (!props.show) {
     return null
@@ -16,12 +30,15 @@ const NewBook = (props) => {
 
     console.log('add book...')
 
+    addBook({ variables: { title, author, published: Number(published), genres } })
+
     setTitle('')
     setPublished('')
     setAuthor('')
     setGenres([])
     setGenre('')
   }
+
 
   const addGenre = () => {
     setGenres(genres.concat(genre))
@@ -30,6 +47,7 @@ const NewBook = (props) => {
 
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <form onSubmit={submit}>
         <div>
           title
@@ -65,6 +83,17 @@ const NewBook = (props) => {
         <div>genres: {genres.join(' ')}</div>
         <button type="submit">create book</button>
       </form>
+    </div>
+  )
+}
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
     </div>
   )
 }
